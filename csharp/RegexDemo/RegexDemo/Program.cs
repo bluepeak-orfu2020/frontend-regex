821a790e32f4bbc6b39091fe5d286c7dd83ae29f
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-04-01 12:30:46 +0200

    Add example code for C#

---
+++ csharp/RegexDemo/RegexDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex("bbb");
+            Console.WriteLine($"is match aaa: {regex.IsMatch("aaa")}");
+            Console.WriteLine($"is match bbb: {regex.IsMatch("bbb")}");
+
+            var match = regex.Match("bbb");
+            Console.WriteLine($"match object: {match}");
+            Console.WriteLine($"match value: {match.Value}");
+
+            var regexWithGroup = new Regex("aa(bb|cc)");
+            var matchWithGroup = regexWithGroup.Match("aacc");
+            Console.WriteLine($"match with group: {matchWithGroup.Groups[1].Value}");
+
+            Console.Read();
+        }
+    }
+}
